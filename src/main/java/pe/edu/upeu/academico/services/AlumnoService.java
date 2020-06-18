c0b07947b1382c59ec4e21e682ad9f0b09f2981e
unknown <richeti1998@gmail.com>
2020-06-18 01:39:15 -0500

    dos

--- src/main/java/pe/edu/upeu/academico/services/AlumnoService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.academico.services;
-
-import java.util.List;
-
-import pe.edu.upeu.academico.entity.Alumno;
-
-public interface AlumnoService {
-	public List<Alumno> findAll();
-}
